31e7b7f75042a3f814e57b6c49624c6fdbdb5767
Gavin D'souza <gavin18d@gmail.com>
2020-03-12 17:59:31 +0530

    chore: unused imports and absolute python path added

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 import os
-import sys
-
-python_path = sys.executable
-requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
 print("Checking Dependencies...")
-os.system("{0} -m pip install -q -r {1}".format(python_path, requirements_path))
+os.system("python -m pip install -q -r requirements.txt")
 
 from biometric_easy_installer import main as run_installer
 run_installer()
