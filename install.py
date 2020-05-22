5a0d6df0fd1443a29ae25b4c5019f4e0a5fc7509
Gavin D'souza <gavin18d@gmail.com>
2020-05-22 14:34:03 +0530

    fix: rename files by context

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ import os
 print("Checking Dependencies...")
 os.system("python -m pip install -q -r requirements.txt")
 
-from biometric_easy_installer import main as run_installer
-run_installer()
+from gui import setup_window
+setup_window()
