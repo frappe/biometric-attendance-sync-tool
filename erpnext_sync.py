238474a601b529d6d83a4e0da477e973aa238687
Asmita Hase <asmitahase@gmail.com>
2025-05-23 14:59:40 +0530

    chore: update example string

--- erpnext_sync.py
+++ erpnext_sync.py
@@ -180,7 +180,7 @@ def get_all_attendance_from_device(ip, port=4370, timeout=30, device_id=None, cl
 
 def send_to_erpnext(employee_field_value, timestamp, device_id=None, log_type=None, latitude=None, longitude=None):
     """
-    Example: send_to_erpnext('12349',datetime.datetime.now(),'HO1','IN')
+    Example: send_to_erpnext('12349',datetime.datetime.now(),'HO1','IN',latitude=12.34, longitude=56.78)
     """
     endpoint_app = "hrms" if ERPNEXT_VERSION > 13 else "erpnext"
     url = f"{config.ERPNEXT_URL}/api/method/{endpoint_app}.hr.doctype.employee_checkin.employee_checkin.add_log_based_on_employee_field"
